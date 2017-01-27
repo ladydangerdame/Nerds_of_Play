ba03453cbba2b14db6f2412f556f0e8bbebfecd3
jeanmw <jean.weatherwax@gmail.com>
2017-01-26 16:03:16 -0800

    jean comments

--- db/migrate/20170125023338_create_comments.rb
+++ db/migrate/20170125023338_create_comments.rb
@@ -1,3 +1,6 @@
+#I had an issue with getting your app to run locally because one of your migrations,
+#I think this one, has an issue with the user relation - try to reclone your repo
+#in a new directory and set up your db to reproduce
 class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#It would be good to have some seed data at some point for when you demo this in interviews etc
